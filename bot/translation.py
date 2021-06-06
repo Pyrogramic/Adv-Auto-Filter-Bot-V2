5dcded43db6a0bbb879c642a08c99e232e793374
Vɪᴠᴇᴋ <Vivektvp1@gmail.com>
2021-06-06 07:53:56 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -2,29 +2,24 @@
 # -*- coding: utf-8 -*-
 # (c) @AlbertEinsteinTG
 
-import os
 class Translation(object):
-  
-
+    
     START_TEXT = """<b>Hey {}!!
 Am Just A Advance Auto Filter Bot....😉
 
 You Can't Use Me.I Specially Maded For My Group.
 
- 🕵‍♂ ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ 👉 <a href="https://t.me/THOR_online">ALLU</a></b>"""    
+ 🕵‍♂ ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ 👉 <a href="https://t.me/Thor_Online">ALLU</a></b>"""    
     
     HELP_TEXT = """
-**നീ ഏതാ..... ഒന്ന് പോടെയ് അവൻ help ചോയ്ച്ച് വന്നിരിക്കുന്നു😤...I'm Different Bot U Know**
+**My Owners Can Only See This 😋**
 """
     
-    ABOUT_TEXT = """**➥ Name** : __Auto Filter Bot__
+    ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
     
-**➥ Creator :** __[ViveK](https://t.me/Vivek_Kerala)__
+<b>➥ Creator</b> : <b><i><a href="https://t.me/Vivek_Kerala">Vivek</a></i></b>
 
-**➥ Language :** __Python3__
+<b>➥ Language</b> : <code>Python3</code>
 
-**➥ Library :** __[Pyrogram Asyncio 1.13.0](https://docs.pyrogram.org)__
-
-
-**Note :** നിങ്ങൾക്ക് എന്നെ വേറെ ഒരിടത്തും ഉപയോഗിക്കാൻ പറ്റൂല വേണേൽ നിങ്ങൾക്ക് Start,Help,About ഒക്കെ അടിച്ച് കളിക്കാം അത്ര മാത്രം..ഇത്രേ എനിക്ക് ചെയ്ത് തരാൻ പറ്റൂ🤭
+<b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
 """
